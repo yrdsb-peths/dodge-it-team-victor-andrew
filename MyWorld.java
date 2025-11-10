e83ee6669c52bfc0e11865b305350b38524047ba
Victor321-coder <440004873@gapps.yrdsb.ca>
2025-11-10 14:33:59 -0500

    updated world to fit through

--- MyWorld.java
+++ MyWorld.java
@@ -6,5 +6,8 @@ public class MyWorld extends World {
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        GreenApple greenApple = new GreenApple();
+        addObject(greenApple, 300, 300);
     }
 }
